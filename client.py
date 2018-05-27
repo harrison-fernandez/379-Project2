856fd8e5a6fa56526769c1f5c7a91e7496f59a67
Harrison Fernandez <harrison.fernandez@jjay.cuny.edu>
2018-05-27 16:03:45 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -12,31 +12,30 @@ def main():
     print("Remote host: {}, remote TCP port: {}".format(hostname, serverTCPPort))
 
     # Prompt user for their name
-    username = input("Please enter your lovely name: ") #A.A
-
+    name = input("Please enter your name: ") #has to be 'start', then type 'start' again to receive instructions to play.
     # Create TCP socket
-    TCPclientsocket = socket(AF_INET, SOCK_STREAM) #A.A
+    clientsocketTCP = socket(AF_INET, SOCK_STREAM)
 
     # Get IP address of server via DNS and print it
-    TCPaddr = (hostname, serverTCPPort) #AA
-    print('The server address is {}:{}'.format(TCPaddr[0], TCPaddr[1])) #AA
+    TCPaddr = (hostname, serverTCPPort)
+    print('The server address is {}:{}'.format(TCPaddr[0], TCPaddr[1]))
     
     # Connect to the server program
-    TCPclientsocket.connect(TCPaddr) #AA
+    clientsocketTCP.connect(TCPaddr)
 
     # Send hello message to the server over TCP connection
-    TCPclientsocket.send("hello {}".format(username).encode()) #AA
+    clientsocketTCP.send("hello {}".format(name).encode())
 
     # TCP Loop
     while True:
         # Read in from TCP port
-        input1 = TCPclientsocket.recv(2048) #aa 
+        gameinput = clientsocketTCP.recv(1024)
 
-        # Keep listening if it doesn't receive a portUDP message
-        if input1.startswith(b'gameport '):
+        # Keep listening if it doesn't receive a gameport message
+        if gameinput.startswith(B'gameport'):
             # Read the control message from the TCP socket and print its contents
             # Split message in two words separated by space, assign the second word to UDPport
-            _, UDPport = input1.decode().split(' ')
+            _, UDPport = gameinput.decode().split(' ')
             UDPport = int(UDPport)
             print('Received UDP port#: {}'.format(UDPport))
             # Break from loop once needed info is received
@@ -44,7 +43,7 @@ def main():
 
 
     # Create a UDP socket
-    UDPsocket = socket(AF_INET, SOCK_DGRAM) #AA A- Not sure if this should be part fo the loop above
+    UDPsocket = socket(AF_INET, SOCK_DGRAM)
     UDPaddr = (hostname, UDPport)
     print(UDPaddr)
     end = False # default end flag
@@ -54,60 +53,59 @@ def main():
         # Prompt
         command = input(">")
         valid_commands = ['start', 'end', 'guess', 'exit']
-        # Validate the command
+    
         if not any(command.startswith(x) for x in valid_commands):
-            # If not valid, ignore and continue asking
+            # If invalid command is entered, ignore and continue asking for input
             continue
 
         # Parse command
         if command.startswith('start'):
-            UDPsocket.sendto(b'ready', UDPaddr)
+            UDPsocket.sendto(B'ready', UDPaddr)
         elif command.startswith('end'):
-            UDPsocket.sendto(b'end', UDPaddr)
+            UDPsocket.sendto(B'end', UDPaddr)
         elif command.startswith('guess'):
             UDPsocket.sendto(command.encode(), UDPaddr)
         elif command.startswith('exit'):
-            UDPsocket.sendto(b'bye', UDPaddr)
+            UDPsocket.sendto(B'bye', UDPaddr)
             end = True
 
         # UDP loop
         while True:
             # Continuously Read in from UDP port
-            incoming, _ = UDPsocket.recvfrom(4096)
+            message, _ = UDPsocket.recvfrom(4096)
 
             # print message
-            # print('Message received: {}'.format(incoming))
+            if message.startswith(B'instr'):
+                text = message[6:] 
+                print(text.decode())
+               
+                message, _ = UDPsocket.recvfrom(4096)
+                _, text1, text2 = message.split(B' ')
+                print('Word: {} Attempts left: {}'.format(text1.decode(), text2.decode()))
 
-            if incoming.startswith(b'instr'):
-                text = incoming[6:]  # Text = message without the first 6 characters ('instr ')
-                print(text.decode())
-                # Instruction message should be followed by stat message
-                incoming, _ = UDPsocket.recvfrom(4096)
-                _, text1, text2 = incoming.split(b' ')
+            elif message.startswith(B'stat'):
+                _, text1, text2 = message.split(B' ')
                 print('Word: {} Attempts left: {}'.format(text1.decode(), text2.decode()))
                 
-            elif incoming.startswith(b'stat'):
-                _, text1, text2 = incoming.split(b' ')
-                print('Word: {} Attempts left: {}'.format(text1.decode(), text2.decode()))
-                
-            elif incoming.startswith(b'end'):
-                text = incoming[4:]
+            elif message.startswith(B'end'):
+                text = message[4:]
                 print(text.decode())
                 
-            elif incoming.startswith(b'bye'):
-                # Send bye message and exit
-                UDPsocket.sendto(b'bye', UDPaddr)
-                end = True
-                
-            elif incoming.startswith(b'na'):
+            elif message.startswith(B'na'):
                 pass
                 
+            elif message.startswith(B'bye'):
+                # Send bye message and exit
+                UDPsocket.sendto(B'bye', UDPaddr)
+                end = True
+                    
             # Break once receiving info and reprompt user
             break
 
         # If end message received, end client process
         if end:
             break
+        #end of Game loop
 
     # Close sockets
     print("Closing TCP and UDP sockets...")
